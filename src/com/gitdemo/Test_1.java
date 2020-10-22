76873f45ca50660f1d5b1a57295471f0c26e59f8
sriyaz <sriyaz@nisum.com>
2020-10-22 19:02:25 +0530

    Commiting Test_2 to BranchX

--- src/com/gitdemo/Test_1.java
+++ src/com/gitdemo/Test_1.java
@@ -4,12 +4,12 @@ public class Test_1 {
 
 	public static void main(String[] args) {
 
-		String hungry = "yes";
+		String hungry = "nO";
 
-		if (hungry == "yes") {
-			System.out.println("Eat something...");
-		} else {
+		if (hungry.equalsIgnoreCase("No")) {
 			System.out.println("Do work...");
+		} else {
+			System.out.println("Eat something...");
 
 		}
 	}
